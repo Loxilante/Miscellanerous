53b54a987fd1449def33aa7bb93f8756aec89ad8
PinkStarSky <50042248+PinkStarSky@users.noreply.github.com>
2020-04-17 22:31:18 +0800

    Update DEBUG.h

--- DEBUG.h
+++ DEBUG.h
@@ -1,31 +1,26 @@
+// DEBUG
+
+// D
 namespace ts
 {
 	string toString(long long e)
 	{
 		bool t = 0;
 		string str;
-		if (e <= 0) t = 1, e = -e;
+		if (e < 0) t = 1, e = -e;
 		do str += '0'+e%10, e /= 10;
 		while(e);
 		reverse(str.begin(), str.end());
 		if (t) str.insert(0, "-");
 		return str += ", ";
 	}
-	string toString(unsigned long long e)
-	{
-		string str;
-		do str += '0'+e%10, e /= 10;
-		while(e);
-		reverse(str.begin(), str.end());
-		return str += ", ";
-	}
 	inline string toString(string s)
 	{
 		return '"' + s + "\", ";
 	}
-	inline string toString(bool b)
+	template <class T, class U> inline string toString(pair<T, U> p)
 	{
-		return b ? "true, " : "false, ";
+		return "(" + toString(p.first) + toString(p.second) + "), ";
 	}
 	template <size_t N> inline string toString(bitset<N> bs)
 	{
@@ -33,11 +28,31 @@ namespace ts
 		for (size_t i = 0; i < N; ++i) str += '0' + bs[i];
 		return str += ", ";
 	}
-	template <class T, class U> inline string toString(pair<T, U> p)
+	template <class C> inline string toString(vector<C> c)
 	{
-		return "(" + toString(p.first) + toString(p.second) + "), ";
+		string str = "{";
+		for (auto& e : c) str += toString(e);
+		return str += "}, ";
 	}
-	template <class C> inline string toString(C c)
+	template <class C> inline string toString(set<C> c)
+	{
+		string str = "{";
+		for (auto& e : c) str += toString(e);
+		return str += "}, ";
+	}
+	template <class C> inline string toString(queue<C> c)
+	{
+		string str = "{";
+		for (auto& e : c) str += toString(e);
+		return str += "}, ";
+	}
+	template <class C> inline string toString(list<C> c)
+	{
+		string str = "{";
+		for (auto& e : c) str += toString(e);
+		return str += "}, ";
+	}
+	template <class T, class U> inline string toString(map<T, U> c)
 	{
 		string str = "{";
 		for (auto& e : c) str += toString(e);
@@ -54,4 +69,14 @@ template <class Head, class... Tail> inline void debug(Head head, Tail... tail)
 	debug(tail...);
 }
 
-#define D(...) cerr<<'['<<#__VA_ARGS__<<"]: ", debug(__VA_ARGS__)
+#define D(...) cerr<<"D:  ["<<#__VA_ARGS__<<"]: ", debug(__VA_ARGS__)
+
+// R
+
+inline void R(ll *t, ll e)
+{
+	ll cnt = 0;
+	cout<<"R:\n\t{";
+	for(ll i = 0; i < e; i++) cout<<*(t+i)<<", ";
+	cout<<"}\n";
+}
